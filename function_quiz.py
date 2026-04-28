ec74c2be2e1ac5e62fd2b7dcebc4e9143a9f4d92
mphvictrsepng-rgb <mphvoctrsepng@gmail.com>
2026-04-28 16:15:03 +0200

    Initial commit. I will push again after completing the quiz

---
+++ function_quiz.py
@@ -0,0 +1,23 @@
+# ---------------------------------------------------------
+# MATH & OOP PYTHON QUIZ
+# ---------------------------------------------------------
+
+# 1. Write a function named 'add' that takes two numbers and returns their sum.
+
+# 2. Write a function named 'minus' that takes two numbers and returns the result.
+
+# 3. Write a function named 'multiply' that takes two numbers and returns the result.
+
+# 4. Write a function named 'divide' that takes two numbers and returns the result.
+
+# 5. Write a function named 'solve_y' that takes m, x, and b, and returns (m * x + b).
+
+# 6. Write a function named 'square' that takes one number and returns that number times itself.
+
+# 7. Create a class named 'MathStudent' that can store a 'name' attribute.
+
+# 8. Inside the 'MathStudent' class, create a method named 'get_area' for a rectangle.
+
+# 9. Create a class named 'Calculator' with a starting attribute 'total' set to 0.
+
+# 10. Inside the 'Calculator' class, create a method named 'add_to_total' to update the total.
---
+++ functions_practice.py
@@ -0,0 +1,19 @@
+#In this file, I learned how to define functions, assign simple parameters with values(arguments), and return values.
+
+"""
+def greet():
+  #print('Hello, welcome to programing.')
+greet()
+"""
+"""
+def greet(name):
+    #print(f'Hello {name}')
+
+greet('Mpho')
+"""
+"""
+def add(a, b):
+    return a - b
+results = add(2, 9)
+print(results)
+"""
