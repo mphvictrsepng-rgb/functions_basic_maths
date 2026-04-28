a7c8c711ae535e6594b99374b34250ab5e8654fb
mphvictrsepng-rgb <mphvoctrsepng@gmail.com>
2026-04-28 19:45:13 +0200

    I completed question 1 till 6. I will complete the rest later on. I just need to read a bit about creating classes before I attempt them.  Additionally, I also included quadratic_function file as part of the class activity we had today (28-04-2026). The code in that file looks cleaner than the one I initially wrote to answer question 1 to 6.

--- function_quiz.py
+++ function_quiz.py
@@ -1,42 +1,43 @@
 # ---------------------------------------------------------
 # MATH & OOP PYTHON QUIZ
 # ---------------------------------------------------------
-"""
+
 # 1. Write a function named 'add' that takes two numbers and returns their sum.
-def add(a, b):
-    return a + b
-results_1 = add(1, 2)
-print(results_1)
+def add(a, b):                  # or can be done this way, but I think the 1st method looks cleaner    # def add(a, b): 
+    results_1 = a + b                                                                                  #    return a + b
+    return print(f'QUESTION 1: {results_1}')                                                           # results_1 = add(1, 2)
+add(1, 3)        # e.g                                                                                 # print(f'QUESTION 1: {results_1}')
 
 # 2. Write a function named 'minus' that takes two numbers and returns the result.
 def minus(a, b):
-    return a - b
-results_2 = minus(3, 1)
-print(results_2)
+    results_2 = a - b
+    return print(f'QUESTION 2: {results_2}')
+minus(4, 1)     # e.g
 
 # 3. Write a function named 'multiply' that takes two numbers and returns the result.
 def multiply(a, b):
-    return a*b
-results_3 = multiply(3, 2)
-print(results_3)
-"""
+    results_3 = a*b
+    return print(f'QUESTION 3: {results_3}')
+multiply(3, 2)    # e.g
+
 # 4. Write a function named 'divide' that takes two numbers and returns the result.
 def divide(a, b):
-    return a / b
-results_4 = int(divide(4, 2))  #print(f'{results_4:.0f}') or int(divide(4, 2)) to return int not float.
-print(results_4)
+    results_4 = a / b                                # I saw that answer returns as float, so I came with 2 ways to get a whole number
+    return print(f'QUESTION 4: {results_4:.0f}')     #print(f'{results_4:.0f}') or int(divide(4, 2)) to return int not float.
+divide(4, 2)        # e.g                                  
 
 # 5. Write a function named 'solve_y' that takes m, x, and b, and returns (m * x + b).
 def solve_y(m, x, b):
-    return m*x + b
-results_5 = solve_y(3, 2, 2)
-print(results_5)
+    results_5 = m*x + b
+    return print(f'QUESTION 5: {results_5}')
+solve_y(2,1,2)      #e.g
 
 # 6. Write a function named 'square' that takes one number and returns that number times itself.
 def square(number):
-    return number*number      # * allows the number to multiply by itself.
-results_6 = square(4)
-print(results_6)
+    results_6 = number*number      # * allows the number to multiply by itself.
+    return print(f'QUESTION 6: {results_6}')
+square(2)
+
 
 # 7. Create a class named 'MathStudent' that can store a 'name' attribute.
 
---
+++ quadratic_function.py
@@ -0,0 +1,11 @@
+#f(x) = ax^2 + bx + c
+#f(x) = 2x^2 + 3x + 4
+
+def quadratic_function(a, b, c, x):
+    results = (a)*(x)*(x) + (b)*(x) + c #return a*x*x + b*x + c
+    return print(results) #quadratic_function(2, 3, 4, -0.75)
+
+quadratic_function(2, 3, 4, -0.75)
+quadratic_function(2, 3, 4, 0)
+
+#(x, y) = (0, 4)
